f25dc1b7abc5363f29847dcded0ebe6fc83e211c
Roshni Swadia <swadia.roshni@gmail.com>
2017-05-14 22:33:13 +0530

    Update HomeControllerTest.java

--- thepizzabox/core/src/test/java/com/pizzaboxcore/controller/HomeControllerTest.java
+++ thepizzabox/core/src/test/java/com/pizzaboxcore/controller/HomeControllerTest.java
@@ -2,6 +2,7 @@ package com.pizzaboxcore.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +17,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.pizzabox.common.constants.JUnitConstants;
+import com.pizzabox.common.model.Order;
+import com.pizzabox.common.model.User;
 import com.pizzabox.common.request.ItemWrapper;
 import com.pizzaboxcore.service.ItemService;
 import com.pizzaboxcore.service.OrderService;
 import com.pizzaboxcore.validator.ItemValidator;
-import com.pizzaboxcore.validator.UserValidator;
+import com.pizzaboxcore.validator.ValidationUtils;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -47,11 +50,11 @@ public class HomeControllerTest {
 	private OrderService orderService;
 
 	@Mock
-	private ItemValidator itemValidator;
+	private ValidationUtils validationUtils;
 	
 	@Mock
-	private UserValidator userValidator;
-
+	private ItemValidator itemValidator;
+	
 	@InjectMocks
 	private HomeController homecontroller;
 
@@ -65,9 +68,11 @@ public class HomeControllerTest {
 
 	@Test
 	public void testShowHomePage() throws Exception {
-		when(itemService.createInitialList()).thenReturn(JUnitConstants.ITEM_LIST);
+		Mockito.when(itemService.createInitialList()).thenReturn(JUnitConstants.ITEM_LIST);
+		
+		Mockito.when(validationUtils.getItemValidator()).thenReturn(itemValidator);
 		Mockito.doNothing().when(itemValidator).validateItemList(JUnitConstants.ITEM_LIST);
-
+		
 		mockMvc.perform(get("/homepage")).andExpect(forwardedUrl("userhomepage"))
 				.andExpect(model().attributeExists("itemWrapper"));
 	}
@@ -80,7 +85,9 @@ public class HomeControllerTest {
 
 		final String[] checkBoxSelected = new String[] { "2", "3" };
 		
-		Mockito.doNothing().when(itemValidator).validateItemWrapper(itemWrapper);
+		Mockito.when(validationUtils.getItemValidator()).thenReturn(itemValidator);
+		Mockito.doNothing().when(itemValidator).validateItemWrapper(Mockito.any(ItemWrapper.class));
+		
 		when(orderService.calculateFinalOrderList(itemWrapper.getItemList(), getCheckBoxList(checkBoxSelected)))
 				.thenReturn(JUnitConstants.FINAL_ITEM_LIST);
 		when(orderService.generateOrder(JUnitConstants.FINAL_ITEM_LIST)).thenReturn(JUnitConstants.ORDER);
