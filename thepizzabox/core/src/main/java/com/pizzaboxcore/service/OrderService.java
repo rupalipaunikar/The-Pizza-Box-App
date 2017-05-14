58a95baa598634576553d2ea8780d624fc17da64
Roshni Swadia <swadia.roshni@gmail.com>
2017-05-14 22:29:54 +0530

    Update OrderService.java

--- thepizzabox/core/src/main/java/com/pizzaboxcore/service/OrderService.java
+++ thepizzabox/core/src/main/java/com/pizzaboxcore/service/OrderService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.pizzabox.common.model.Item;
 import com.pizzabox.common.model.Order;
 import com.pizzabox.common.model.User;
+import com.pizzaboxcore.custom.exception.OrderGenerationException;
+import com.pizzaboxcore.custom.exception.UserNotFoundException;
 
 
 /**
@@ -24,16 +26,19 @@ public interface OrderService {
 	 * @param List<Item> This contains all the items(Pizza,Beverage,Sides)details selected by the user
 	 * @param totalPrice This is the total Price of the order
 	 * @return Order
+	 * @throws OrderGenerationException 
+	 * @throws UserNotFoundException 
 	 */
-	public Order generateOrder(List<Item> itemList);
+	public Order generateOrder(List<Item> itemList) throws OrderGenerationException, UserNotFoundException;
 	
 	/**
 	 * This API is used to get the complete details of the Logged in user 
 	 * 	
 	 * @param user This Principal object contains the name of the logged in user 
 	 * @return User
+	 * @throws UserNotFoundException 
 	 */
-	public User getUserDetails(String username);
+	public User getUserDetails(String username) throws UserNotFoundException;
 	
 	/**
 	 * This API creates a final list of item based on user selection 
